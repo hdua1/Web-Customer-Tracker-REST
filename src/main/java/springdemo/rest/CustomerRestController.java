69f09208df7548323c0773eaa53eabb456096455
hdua1@asu.edu <hdua1@asu.edu>
2018-08-19 16:09:18 -0700

    Added RestController

---
+++ src/main/java/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,25 @@
+package springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springdemo.entity.Customer;
+import springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		
+		return customerService.getCustomers();	
+	}
+}
