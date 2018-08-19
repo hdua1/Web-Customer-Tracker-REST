b0cf6d150fe58077b16ee1ae3199310f992b6374
hdua1@asu.edu <hdua1@asu.edu>
2018-08-19 16:24:43 -0700

    Added ExceptionHandler

--- src/main/java/springdemo/rest/CustomerRestController.java
+++ src/main/java/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,16 @@ public class CustomerRestController {
 		
 		return customerService.getCustomers();
 		
+	}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id:" + customerId + "not found!");
+		}
+		
+		return theCustomer;
 	}
 }
